3d7752bd342bc394f7aa641a17a51ea7aef07862
Taylor D. Brown <ttuck101@gmail.com>
2022-11-01 12:03:44 -0400

    Add fiver to icon list

--- src/js/components/banners/SocialMediaTrayComponent.js
+++ src/js/components/banners/SocialMediaTrayComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import SocialMediaButtonComponent from '../buttons/SocialMediaButtonComponent';
-import facebookSvg from './../../../svg/5365678_fb_facebook_facebook_logo_icon_black.svg'
+
+import facebookSvg from './../../../svg/5365678_fb_facebook_facebook_logo_icon.svg'
 import linkedinSvg from './../../../svg/5296501_linkedin_network_linkedin_logo_icon.svg'
-import githubSvg from './../../../svg/github_black.svg'
+import fiverrSvg from './../../../svg/fiverr.svg'
+import githubSvg from './../../../svg/github.svg'
 
 class SocialMediaTrayComponent extends React.Component {
     constructor(props) {
@@ -18,6 +20,9 @@ class SocialMediaTrayComponent extends React.Component {
                 <SocialMediaButtonComponent
                     imageSrc={githubSvg} 
                     redirectUrl={'https://github.com/TaybrownOB196/'} />
+                <SocialMediaButtonComponent
+                    imageSrc={fiverrSvg}
+                    redirectUrl={'https://www.fiverr.com/snitch196?public_mode=true'} />
                 <SocialMediaButtonComponent
                     imageSrc={facebookSvg} 
                     redirectUrl={'https://www.facebook.com/Taylor.Delbert.Brown/'} />
