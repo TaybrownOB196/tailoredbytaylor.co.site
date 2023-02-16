4d8798a573d76ad87bd57994de8c18fff78d7354
Taylor D. Brown <ttuck101@gmail.com>
2023-02-16 16:40:34 -0500

    Finally fix/implement matrix class

--- src/js/lib/gaming/Matrix.js
+++ src/js/lib/gaming/Matrix.js
@@ -1,99 +1,87 @@
 import Grid from '../grid/Grid';
 
-class Grid {
-    constructor(_default, colCount, rowCount) {
-        
-        if (colCount < 1 || rowCount < 1)
-        throw 'columns/rows must be greater than 1';
-        
-        this.Default = _default;
-        this.ColCount = colCount;
-        this.RowCount = rowCount;
-        this.Container = [];
-        for(let col=0; col<this.ColCount; col++) {
-            let row = [];
-            for (var r=0; r<this.RowCount; r++) {
-                row.push(this.Default);
-            }
-
-            this.Container.push(row);
-        }
-    }
-
-    ///Returning false from the action will short cirtuit the rest of the loop
-    ExecuteGrid(action) {
-        let _continue = true; 
-        for(let c = 0; c<this.ColCount && _continue; c++) {
-            for(let r = 0; r<this.RowCount && _continue; r++) {
-                let res = action(r, c);
-                if (res === true)
-                    _continue = false;
-            }
-        }
-    }
-    ExecuteGridR(action) {
-        let _continue = true; 
-        for(let r = 0; r<this.RowCount && _continue; r++) {
-            for(let c = 0; c<this.ColCount && _continue; c++) {
-                let res = action(r, c);
-                if (res === true)
-                    _continue = false;
-            }
-        }
-    }
-    Set(col, row, obj) { this.Container[col][row] = obj; }
-    SetR(col, row, obj) { this.Container[row][col] = obj; }
-    Get(col, row) { return this.Container[col][row]; }
-    GetR(col, row) { return this.Container[row][col]; }
-}
-
-//TODO: Reform logic into a transform class for a vector2d using an adapter style library or something
+//TODO: Reform logic into a transform class for a vector2d 
+//using an adapter style library or something
 class Matrix2d {
-    constructor(col, row) {
-        this.grid = new Grid(0, col, row);
+    constructor(row, col) {
+        this.grid = new Grid(0, row, col);
+        this.RowCount = row;
+        this.ColCount = col;
     }
 
-    set(col, row, value) {
-        this.grid.Set(col, row, value);
+    getRow(row) {
+        return this.grid.Container[row];
     }
 
-    get(col, row) {
-        return this.grid.Get(col, row);
+    getColumn(col) {
+        let toReturn = [];
+        for (let idx=0; idx<this.RowCount; idx++) {
+            toReturn.push(this.grid.Container[idx][col]);
+        }
+
+        return toReturn;
+    }
+
+    set(row, col, value) {
+        this.grid.SetR(col, row, value);
+    }
+
+    get(row, col) {
+        return this.grid.GetR(col, row);
     }
 
     multiply(otherMatrix) {
-        if (this.grid.ColCount !== otherMatrix.grid.RowCount) 
-            throw new Exception(`unable to multiply due to dimensions ${this.grid.ColCount} ${otherMatrix.grid.RowCount}`);
-        let matrixIdx = 0;
-        let cellSum = 0;
-        let toReturn = new Matrix2d(this.grid.RowCount, otherMatrix.grid.ColCount);
-        console.log(this.grid.RowCount)
-        for (let idx1=0; idx1<toReturn.grid.ColCount; idx1++) {
-            for (let idx2=0; idx2<toReturn.grid.RowCount; idx2++) {
-                while (matrixIdx < otherMatrix.grid.RowCount) {
-                    try {
-                        cellSum += this.get(idx1, matrixIdx) * otherMatrix.get(idx2, matrixIdx);
-                        console.log(matrixIdx, idx1, idx2);
-                        console.log(`${this.get(idx1, matrixIdx)} * ${otherMatrix.get(idx2, matrixIdx)}`);
-                        matrixIdx++;
-                    } catch(err) {
-                        console.log(err);
-                        throw err;
-                    }
+        if (this.ColCount !== otherMatrix.RowCount) 
+            throw new Exception(`unable to multiply due to dimensions ${this.ColCount} ${otherMatrix.RowCount}`);
+        let toReturn = new Matrix2d(this.RowCount, otherMatrix.ColCount);
+        for (let idx1=0; idx1<this.RowCount; idx1++) {
+            for (let idx2=0; idx2<otherMatrix.ColCount; idx2++) {
+                let row = this.getRow(idx1);
+                let column = otherMatrix.getColumn(idx2);
+                let value = 0;
+                let str = '';
+                for (let idx3=0; idx3<row.length; idx3++) {
+                    if (idx3 != 0) str += '+'
+                    str += `${row[idx3]} * ${column[idx3]} `;
+                    value += row[idx3] * column[idx3];
                 }
-                matrixIdx = 0;
-                toReturn.set(idx1, idx2, cellSum);
+
+                toReturn.set(idx1, idx2, value);
             }
         }
 
         return toReturn;
     }
 
-    print() {
-        let toReturn = "";
-        this.gri
-        return toReturn;
+    _print() {
+        let row = '';
+        this.grid.ExecuteGrid((c,r) => {
+            row += `${this.get(r,c)} `;
+            if (c >= this.ColCount-1) {
+                console.log(row);
+                row = '';
+            }
+        });
     }
 }
+
+// let a = new Matrix2d(2,3);
+// let b = new Matrix2d(3,2);
+
+// a.set(0,0, 1);
+// a.set(1,0, 1);
+// a.set(0,1, 4);
+// a.set(1,1, 2);
+// a.set(0,2, 1);
+// a.set(1,2, 1);
+
+// b.set(0,0, 1);
+// b.set(1,0, 4);
+// b.set(2,0, 1);
+
+// a._print()
+// console.log(b.grid.Container)
+
+// a.multiply(b).grid.Container;
 
 export default Matrix2d;  
